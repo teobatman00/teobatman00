28b1cfa8625d46d36ab8f1829c3165296ea4d862
teobatman00 <teobatman00@gmail.com>
2021-07-14 21:21:48 +0700

    update

--- js/index.js
+++ js/index.js
@@ -11,8 +11,29 @@ $(document).ready(function(){
             items:3
         },
         1000:{
-            items:5
+            items:5 
         }
     }
   });
+
+  $('.lazy').Lazy({
+    scrollDirection: 'vertical',
+    effect: 'fadeIn',
+    effectTime: 1500,
+    visibleOnly: true,
+    onError: function(element) {
+        console.log('error loading ' + element.data('src'));
+    }
+  });
+
+  $(window).scroll(function(){
+    $('section:not(section:first-child)').find('.container').each(function(e){
+      var bottomElement = $(this).offset().top + $(this).outerHeight();
+      var bottomWindow = $(window).scrollTop() + $(window).height();
+      if(bottomWindow > bottomElement-150){
+        $(this).animate({"opacity":"1"},500);
+      }
+    });
+  });
+
 });
